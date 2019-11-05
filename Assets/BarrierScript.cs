a6d5808afc48ba11e1edf41a49a0f4f39f255abb
FlamingSparrow <nehaltangudu12@gmail.com>
2019-11-04 19:03:44 -0600

    Add files via upload

---
+++ Assets/BarrierScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Camera cam;
+    private string heightorwidth;
+
+    private void Start()
+    {
+        heightorwidth = "Height";
+    }
+
+    void FixedUpdate()
+    {
+        // Right click to change whether you want to change height or width.
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (heightorwidth == "Height")
+            {
+                heightorwidth = "Width";
+            }
+            else
+            {
+                heightorwidth = "Height";
+            }
+        }
+
+        // Press space and move mouse to move screen. Also, scroll the mouse wheel when pressing square to edit height/width.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.MovePosition(cam.ScreenToWorldPoint(Input.mousePosition));
+
+            if (heightorwidth == "Height")
+            {
+                transform.localScale += new Vector3(0, Input.mouseScrollDelta.y, 0);
+            }
+            else
+            {
+                transform.localScale += new Vector3(Input.mouseScrollDelta.y, 0, 0);
+            }
+        }
+
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpHeight;
+    public Rigidbody2D rb;
+    private Vector3 startPosition;
+    
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            rb.AddForce(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-speed, 0,0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(0, jumpHeight,0));
+        }
+    }
+}
