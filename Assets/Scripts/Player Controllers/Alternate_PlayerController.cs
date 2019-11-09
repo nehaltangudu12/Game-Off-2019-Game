b1f93325a3279e8b8e5f2290e82229116e48c3ac
FlamingSparrow <nehaltangudu12@gmail.com>
2019-11-09 12:46:56 -0600

    Added a freeze effect with space and tried (and failed) to do walljumps

--- Assets/Scripts/Player Controllers/Alternate_PlayerController.cs
+++ Assets/Scripts/Player Controllers/Alternate_PlayerController.cs
@@ -9,6 +9,7 @@ public class Alternate_PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     bool grounded = true;
     private Vector3 startPosition;
+    private Vector2 velocitybeforefrozen;
 
     // Start is called before the first frame update
     void Start()
@@ -46,19 +47,38 @@ public class Alternate_PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-            if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-speed, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(1.0f,0.0f,0.0f))))
             {
-                rb.AddForce(new Vector3(speed, 0, 0));
+                rb.AddForce(new Vector3(-(jumpHeight*10), (jumpHeight * 10), 0));
             }
-            if (Input.GetKey(KeyCode.LeftArrow))
+            else if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(-1.0f, 0.0f, 0.0f))))
             {
-                rb.AddForce(new Vector3(-speed, 0, 0));
+                rb.AddForce(new Vector3(jumpHeight * 10, (jumpHeight * 10), 0));
             }
-            if (Input.GetKeyDown(KeyCode.UpArrow) && grounded == true)
+            else
             {
                 rb.AddForce(new Vector3(0, (jumpHeight * 10), 0));
             }
-     
+        } 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocitybeforefrozen = rb.GetPointVelocity(rb.transform.position);
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.AddForce(velocitybeforefrozen);
+        }
     }
 }
