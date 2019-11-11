7a17aa59c4ef58ff5fb8ee01a669ce94da14d502
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-11 10:17:33 +0100

    Organizing scripts and test snapping to a Grid's chunk

---
+++ Assets/Scripts/Camera/TileMapController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class TileMapController : Singleton<TileMapController>
+{
+    private Grid _tilesGrid;
+    public Grid TilesGrid => _tilesGrid;
+
+    private TilemapRenderer _tileMapRend;
+    public TilemapRenderer TileMapRend => _tileMapRend;
+
+    void Start ()
+    {
+        TryGetComponent (out _tileMapRend);
+        transform.parent.TryGetComponent (out _tilesGrid);
+    }
+}
