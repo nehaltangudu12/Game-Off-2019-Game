e7759374dfd6af20b5ad3749f66ead21f95cc15b
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-11 15:07:05 +0100

    Move camera's bounds with arrow keys

--- Assets/Scripts/Camera/CameraTest.cs
+++ Assets/Scripts/Camera/CameraTest.cs
@@ -1,78 +1,99 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿    using DG.Tweening;
+    using GhAyoub.InputSystem;
+    using UnityEngine;
 
-public class CameraTest : MonoBehaviour
-{
-    [SerializeField] private float TimeToSnap = 0.25f;
-
-    private Camera _camera;
-
-    private Grid _tilesMapGrid;
-
-    public void Init ()
+    public class CameraTest : MonoBehaviour
     {
-        TryGetComponent (out _camera);
+        [SerializeField] private float TimeToSnap = 0.25f;
+        [SerializeField] private float CamMoveStep = 1.5f;
 
-        _tilesMapGrid = TileMapController.Instance.TilesGrid;
-    }
+        private Camera _camera;
 
-    private int index = 0;
-    void Update ()
-    {
-        if (Input.GetKeyDown (KeyCode.F2))
+        private Grid _tilesMapGrid;
+        private InputData _inputData;
+
+        public void Init ()
         {
-            var pos = new Vector3 (0f, 0f, -200f);
-            transform.DOMove (pos, TimeToSnap);
-            _camera.DOOrthoSize (24f, TimeToSnap);
+            TryGetComponent (out _camera);
+
+            _tilesMapGrid = TileMapController.Instance.TilesGrid;
+            _inputData = PlayerInput.Instance.Data;
         }
 
-        if (Input.GetKeyUp (KeyCode.F2))
+        private int index = 0;
+        void Update ()
         {
-            var pos0 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 0, 0));
-            var posing0 = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
-            transform.DOMove (posing0, TimeToSnap, true);
-            _camera.DOOrthoSize (12f, TimeToSnap);
-        }
-
-        if (Input.GetKeyDown (KeyCode.F1))
-        {
-            var pos0 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 0, 0));
-            var pos1 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 1, 0));
-            var pos2 = _tilesMapGrid.CellToWorld (new Vector3Int (1, 0, 0));
-            var pos3 = _tilesMapGrid.CellToWorld (new Vector3Int (1, 1, 0));
-
-            switch (index)
+            if (_inputData.CamArrowUp)
             {
-                case 0:
-                    var posing = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
-                    transform.DOMove (posing, TimeToSnap, true);
-                    break;
-
-                case 1:
-                    var posing1 = new Vector3 (pos1.x - 21f, pos1.y - 12f, -200);
-                    transform.DOMove (posing1, TimeToSnap, true);
-                    break;
-
-                case 2:
-                    var posing2 = new Vector3 (pos2.x - 21f, pos2.y - 12f, -200);
-                    transform.DOMove (posing2, TimeToSnap, true);
-                    break;
-
-                case 3:
-                    var posing3 = new Vector3 (pos3.x / 2, pos3.y / 2, -200);
-                    transform.DOMove (posing3, TimeToSnap, true);
-                    break;
-
-                default:
-                    index = 0;
-                    var defaultPos = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
-                    transform.DOMove (defaultPos, TimeToSnap, true);
-                    break;
+                transform.DOMoveY (Mathf.Clamp (transform.position.y + CamMoveStep, -12f, 12f), TimeToSnap);
             }
-            
-            _camera.DOOrthoSize (12f, TimeToSnap);
+            else if (_inputData.CamArrowDown)
+            {
+                transform.DOMoveY (Mathf.Clamp (transform.position.y - CamMoveStep, -12f, 12f), TimeToSnap);
+            }
+            else if (_inputData.CamArrowLeft)
+            {
+                transform.DOMoveX (Mathf.Clamp (transform.position.x - CamMoveStep, -21f, 21f), TimeToSnap);
+            }
+            else if (_inputData.CamArrowRight)
+            {
+                transform.DOMoveX (Mathf.Clamp (transform.position.x + CamMoveStep, -21f, 21f), TimeToSnap);
+            }
 
-            index++;
+            if (Input.GetKeyDown (KeyCode.F2))
+            {
+                var pos = new Vector3 (0f, 0f, -200f);
+                transform.DOMove (pos, TimeToSnap);
+                _camera.DOOrthoSize (24f, TimeToSnap);
+            }
+
+            if (Input.GetKeyUp (KeyCode.F2))
+            {
+                var pos0 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 0, 0));
+                var posing0 = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
+                transform.DOMove (posing0, TimeToSnap, true);
+                _camera.DOOrthoSize (12f, TimeToSnap);
+            }
+
+            if (Input.GetKeyDown (KeyCode.F1))
+            {
+                var pos0 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 0, 0));
+                var pos1 = _tilesMapGrid.CellToWorld (new Vector3Int (0, 1, 0));
+                var pos2 = _tilesMapGrid.CellToWorld (new Vector3Int (1, 0, 0));
+                var pos3 = _tilesMapGrid.CellToWorld (new Vector3Int (1, 1, 0));
+
+                switch (index)
+                {
+                    case 0:
+                        var posing = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
+                        transform.DOMove (posing, TimeToSnap, true);
+                        break;
+
+                    case 1:
+                        var posing1 = new Vector3 (pos1.x - 21f, pos1.y - 12f, -200);
+                        transform.DOMove (posing1, TimeToSnap, true);
+                        break;
+
+                    case 2:
+                        var posing2 = new Vector3 (pos2.x - 21f, pos2.y - 12f, -200);
+                        transform.DOMove (posing2, TimeToSnap, true);
+                        break;
+
+                    case 3:
+                        var posing3 = new Vector3 (pos3.x / 2, pos3.y / 2, -200);
+                        transform.DOMove (posing3, TimeToSnap, true);
+                        break;
+
+                    default:
+                        index = 0;
+                        var defaultPos = new Vector3 (pos0.x - 21f, pos0.y - 12f, -200);
+                        transform.DOMove (defaultPos, TimeToSnap, true);
+                        break;
+                }
+
+                _camera.DOOrthoSize (12f, TimeToSnap);
+
+                index++;
+            }
         }
     }
-}
--- Assets/Scripts/InputSystem/KeyboardLayout.cs
+++ Assets/Scripts/InputSystem/KeyboardLayout.cs
@@ -5,7 +5,11 @@ namespace GhAyoub.InputSystem
     public class KeyboardLayout : ILayout
     {
         private KeyCode _kcJump = KeyCode.Space;
-
+        private KeyCode _kcCamLeft = KeyCode.LeftArrow;
+        private KeyCode _kcCamRight = KeyCode.RightArrow;
+        private KeyCode _kcCamUp = KeyCode.UpArrow;
+        private KeyCode _kcCamDown = KeyCode.DownArrow;
+        
         public void Execute (InputData data)
         {
             var xMove = Input.GetAxisRaw ("Horizontal");
@@ -13,6 +17,10 @@ namespace GhAyoub.InputSystem
             data.XMove += xMove;
 
             data.Jump |= Input.GetKeyDown(_kcJump);
+            data.CamArrowUp |= Input.GetKeyDown(_kcCamUp);
+            data.CamArrowDown |= Input.GetKeyDown(_kcCamDown);
+            data.CamArrowLeft |= Input.GetKeyDown(_kcCamLeft);
+            data.CamArrowRight |= Input.GetKeyDown(_kcCamRight);
         }
     }
 }
