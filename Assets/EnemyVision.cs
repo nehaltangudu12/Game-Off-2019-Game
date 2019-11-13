887c32e385421490119092fac784736b6d198769
FlamingSparrow <nehaltangudu12@gmail.com>
2019-11-12 18:35:28 -0600

    added spikes

---
+++ Assets/EnemyVision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+    public Collider2D Vision;
+
+    private Collider2D _playerCollider;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var player = GameObject.FindWithTag("Player");
+        
+        if(player == null)
+            Debug.LogError("Can't find game object with 'Player' tag!");
+        
+        _playerCollider = player.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsPlayerVisible();
+    }
+
+    private bool IsPlayerVisible()
+    {
+        if(Vision.IsTouching(_playerCollider))
+            Debug.Log("Touching");
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Global/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    public class Settings
+    {
+        public static float PixelsInGridX {
+            get
+            {
+                Debug.LogWarning("Please delete this property and all calls right after Level Editor is completed.");
+                return 1.26f;
+            }
+        }
+        
+        public static float PixelsInGridY {
+            get
+            {
+                Debug.LogWarning("Please delete this property and all calls right after Level Editor is completed.");
+                return 0.72f;
+            }
+        }
+    }
+}
