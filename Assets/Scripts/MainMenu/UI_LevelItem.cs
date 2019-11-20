321b8434ec9798ccdc5b003b2b9044592bab2098
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-20 13:12:59 +0100

    Working on the main menu

---
+++ Assets/Scripts/MainMenu/UI_LevelItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LevelItem : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer VisualLevelItem = null;
+    [SerializeField] private SpriteRenderer VisualLevelContent = null;
+
+    public Vector2 Size => VisualLevelItem.bounds.size;
+
+    public bool IsHovered { get; private set; }
+
+    private Color _initColor = Color.black;
+
+    public void Init ()
+    {
+        _initColor = VisualLevelContent.color;
+    }
+
+    public void UpdateItem (bool isHovered)
+    {
+        VisualLevelContent.color = isHovered ? Color.white : _initColor;
+    }
+}
---
+++ Assets/Scripts/MainMenu/UI_LevelsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GhAyoub.InputSystem;
+using UnityEngine;
+
+public class UI_LevelsController : MonoBehaviour
+{
+    [SerializeField] private UI_LevelItem[] LevelItems;
+
+    private PlayerInput _inputInstance;
+
+    private void Start ()
+    {
+        _inputInstance = PlayerInput.Instance;
+
+        foreach (var item in LevelItems)
+        {
+            item.Init ();
+        }
+    }
+
+    void Update ()
+    {
+        var mousePos = _inputInstance.Data.MousePosition;
+        for (int i = 0; i < LevelItems.Length; i++)
+        {
+            var item = LevelItems[i];
+            var itemPos = LevelItems[i].transform.position;
+            var ishovered = mousePos.x < itemPos.x && mousePos.x > itemPos.x - item.Size.x;
+
+            item.UpdateItem (ishovered);
+        }
+    }
+}
