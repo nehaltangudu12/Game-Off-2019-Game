a9c8f532b7b31d13a4f68804418d0d19f0f3d0c7
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-20 20:21:53 +0100

    Depth of field effect when loading level

--- Assets/Scripts/Camera/CameraEffects.cs
+++ Assets/Scripts/Camera/CameraEffects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
@@ -8,14 +9,23 @@ public class CameraEffects : MonoBehaviour
     [SerializeField] private PostProcessVolume PostVolume;
 
     private LensDistortion _currentLensDistortion = null;
+    private DepthOfField _currentDepthOfField = null;
 
     void Start ()
     {
+        PostVolume.profile.TryGetSettings (out _currentDepthOfField);
         PostVolume.profile.TryGetSettings (out _currentLensDistortion);
+
+        DepthOfFieldAnim ();
+    }
+
+    void DepthOfFieldAnim ()
+    {
+        DOTween.To (() => { return _currentDepthOfField.aperture.value; }, val => { _currentDepthOfField.aperture.value = val; }, 31f, 3);
     }
 
     public void LensDistortionStatus (bool enable)
     {
-        _currentLensDistortion.active = enable; 
+        _currentLensDistortion.active = enable;
     }
 }
