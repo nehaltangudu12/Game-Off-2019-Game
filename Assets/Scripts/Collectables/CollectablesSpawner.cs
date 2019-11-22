598d6e1f81829372e853524ca195320b9dfcbdf5
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-22 23:37:24 +0100

    Draft - Collectables

---
+++ Assets/Scripts/Collectables/CollectablesSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablesSpawner : MonoBehaviour
+{
+    [SerializeField] private BatteryCollectable BCollectable;
+    [SerializeField] private Transform[] SpawnPoints;
+
+    private List<ICollectable> _collectables = new List<ICollectable> ();
+
+    void Start ()
+    {
+        SpawnCollectables ();
+    }
+
+    void SpawnCollectables ()
+    {
+        for (int i = 0; i < SpawnPoints.Length; i++)
+        {
+            var collectable = Instantiate (BCollectable, SpawnPoints[i].position, Quaternion.identity);
+            collectable.transform.SetParent (this.transform);
+            collectable.Init (this);
+            _collectables.Add (collectable);
+        }
+    }
+
+    internal void Collect (ICollectable collectable)
+    {
+        collectable.Collect ();
+        ConsumeCollectable (collectable);
+    }
+
+    void ConsumeCollectable (ICollectable collectable)
+    {
+        var index = _collectables.IndexOf (collectable);
+
+        if (index != -1)
+        {
+            _collectables.RemoveAt (index);
+            Destroy (collectable.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Collectables/ICollectable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor;
+using UnityEngine;
+
+public class ICollectable : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer CollectableVisual;
+
+    private Sequence _animSeq;
+    private CollectablesSpawner _spawner;
+
+    public void Init (CollectablesSpawner spawner)
+    {
+        _spawner = spawner;
+        _animSeq = DOTween.Sequence();
+
+        Animate ();
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag ("Player"))
+        {
+            _spawner.Collect(this);
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.collider.CompareTag ("Player"))
+        {
+             _spawner.Collect(this);
+        }
+    }
+
+    public void Collect ()
+    {
+        Debug.Log("Effect here");
+    }
+
+    void Animate ()
+    {
+        _animSeq.Append(CollectableVisual.transform.DOShakePosition (.25f, .5f, 3, 20).SetDelay(1.5f)).SetLoops(-1);
+
+        _animSeq.Play();
+    }
+}
