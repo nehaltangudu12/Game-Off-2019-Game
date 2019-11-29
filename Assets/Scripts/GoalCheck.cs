3f1bb4bbd2d6dda8f9967d5046ffcca97bcf4af6
FlamingSparrow <nehaltangudu12@gmail.com>
2019-11-29 15:15:37 -0600

    Added a goal script

---
+++ Assets/Scripts/GoalCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class GoalCheck : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
