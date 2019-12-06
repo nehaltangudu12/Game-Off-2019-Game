5dd1f47d0fd42c9cc8f72edd9dd9d517aaf42e39
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-12-06 12:14:23 +0100

    Fix character controller wrong sliding directions

--- Assets/Scripts/PlayerV2/CharacterController.cs
+++ Assets/Scripts/PlayerV2/CharacterController.cs
@@ -27,7 +27,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private CharacterSound CharSounds = null;
 
     private bool _isGrounded = false;
-    private bool _isNearWall = false;
+    private bool _isNearWallFromRight = false;
+    private bool _isNearWallFromLeft = false;
     private bool _isWallSliding = false;
     private bool _isFacingRight = true;
     private InputData _inputData = null;
@@ -49,7 +50,7 @@ public class CharacterController : MonoBehaviour
         _inputData = PlayerInput.Instance.Data;
         _debugStyle = DebugGUISkin.label;
 
-        CamController.Init (this);
+        CamController?.Init (this);
     }
     #region  Debug
 
@@ -63,7 +64,7 @@ public class CharacterController : MonoBehaviour
 
         var wallRect = new Rect (50f, 110f, 250f, 50f);
 
-        GUI.Label (wallRect, string.Format ("Near Wall? => {0}", _isNearWall), _debugStyle);
+        // GUI.Label (wallRect, string.Format ("Near Wall? => {0}", _isNearWall), _debugStyle);
 
         var wallSlideRect = new Rect (50f, 170f, 250f, 50f);
 
@@ -138,7 +139,7 @@ public class CharacterController : MonoBehaviour
 
     void WallSide ()
     {
-        _isWallSliding = (_rdPlayer.velocity.y < 0 && _isNearWall && !_isGrounded);
+        _isWallSliding = (_rdPlayer.velocity.y < 0 && (_isNearWallFromLeft || _isNearWallFromRight) && !_isGrounded);
     }
 
     void Movement ()
@@ -190,9 +191,9 @@ public class CharacterController : MonoBehaviour
     {
         _isGrounded = Physics2D.OverlapCircle (GroundCheckObj.position, GroundCheckRadius, GroundLayer);
 
-        _isNearWall = Physics2D.Raycast (WallCheckObj.position, transform.right, WallCheckDistRight, WallLayer | BoundsLayer) ||
-            Physics2D.Raycast (WallCheckObj.position, -transform.right, WallCheckDistLeft, WallLayer | BoundsLayer);
+        _isNearWallFromRight = Physics2D.Raycast (WallCheckObj.position, transform.right, WallCheckDistRight, WallLayer | BoundsLayer);
 
+        _isNearWallFromLeft = Physics2D.Raycast (WallCheckObj.position, -transform.right, WallCheckDistLeft, WallLayer | BoundsLayer);
     }
 
     void FlipIt ()
@@ -207,8 +208,25 @@ public class CharacterController : MonoBehaviour
             {
                 _isFacingRight = true;
             }
-        }
 
-        transform.localScale = new Vector3 (_isFacingRight ? (_isWallSliding? - 1.5f : 1.5f) : (_isWallSliding? 1.5f: -1.5f), 1.5f, 1.5f);
+            transform.localScale = new Vector3 (_isFacingRight ? 1.5f : -1.5f, 1.5f, 1.5f);
+        }
+        else // if sliding
+        {
+            if (_isNearWallFromLeft)
+            {
+                if (!_isFacingRight)
+                {
+                    transform.localScale = new Vector3 (1.5f, 1.5f, 1.5f);
+                }
+            }
+            else if (_isNearWallFromRight)
+            {
+                if (_isFacingRight)
+                {
+                    transform.localScale = new Vector3 (-1.5f, 1.5f, 1.5f);
+                }
+            }
+        }
     }
 }
